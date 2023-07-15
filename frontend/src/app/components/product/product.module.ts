2f9840a04148853adfa60ee7a4ecaf7d645676ab
Revathi <revathimeenashi@gmail.com>
2023-07-15 20:57:02 +0530

    hierarchy fix

--- frontend/src/app/components/product/product.module.ts
+++ frontend/src/app/components/product/product.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductItemComponent } from './product-list/product-item/product-item.component';
 import { ProductRoutingModule } from './product-routing.module';
 
 //Material Component
